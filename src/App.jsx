147eda06cbb52b2b100139510af8e29f8bfdb74a
kawtaraitsaid <156421778+kawtaraitsaid@users.noreply.github.com>
2024-07-15 11:45:18 +0200

    building table in react

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import TodoList from "./TodoList";
+import DataTable from "./table";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -31,6 +32,7 @@ function App() {
         <button className="btn2">Edit</button>
       </div> */}
       {TodoList()}
+      <DataTable />
     </>
   );
 }
